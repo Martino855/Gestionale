0de10612e2587c03918828b175f02e8262f93ba9
Alessandro <nathannemesis2@gmail.com>
2023-09-19 15:20:06 +0200

    creato componenti

--- src/app/components/private/private.module.ts
+++ src/app/components/private/private.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserdetailComponent } from './userdetail/userdetail.component';
+import { CompanyComponent } from './company/company.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    UsersComponent,
+    UserdetailComponent,
+    CompanyComponent
+  ],
   imports: [
     CommonModule,
     PrivateRoutingModule
--- src/app/components/public/public.module.ts
+++ src/app/components/public/public.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    NotFoundComponent,
+    SubscribeComponent
+  ],
   imports: [
     CommonModule,
     PublicRoutingModule
